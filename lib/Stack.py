4826b6f4f5304c9b055927ba862f960fe5e4e4a9
waynetravis <waynetavis123@gmail.com>
2025-08-26 11:58:42 +0300

    commit changes made

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
+
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items[:limit] 
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+    
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        for i in range(len(self.items)-1, -1, -1):
+            if self.items[i] == target:
+                return len(self.items) - i - 1
+        return -1
